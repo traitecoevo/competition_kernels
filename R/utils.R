e3d58d115a2adf36bf752609c46f4e30616359a4
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-03-10 12:53:45 +1100

    New build system for latex files

--- R/utils.R
+++ R/utils.R
@@ -16,19 +16,42 @@ build_pdf <- function(texfile) {
   owd <- setwd("ms")
   on.exit(setwd(owd))
   texfile <- basename(texfile)
-  command <- sprintf('latexmk -pdf -pdflatex=\"%s\" %s',
-                     "pdflatex -interaction=nonstopmode", texfile)
-  res <- system(command, intern=FALSE)
-  # TODO: how to catch errors? Above does not quite work because res behaves
-  # strangely (no contents, not length)
-  #	res <- system(command, intern=TRUE)
-  # code <- attr(res, "status")
-  # browser()
-  # if (code != 0L) {
-  # 	stop("Error running latexmk: ", as.character(attr(res, "errmsg", TRUE)))
-  # }
+
+  latex <- Sys.which("pdflatex")
+  bibtex <- Sys.which("bibtex")
+
+  if (latex == "" || bibtex == "") {
+    stop("latex / bibtex not found in path")
+  }
+
+  ## These can be generalised a bunch:
+  run_latex <- function() {
+    args <- c("-interaction=nonstopmode", texfile)
+    res <- suppressWarnings(system2(latex, args, stdout=TRUE, stderr=TRUE))
+    code <- attr(res, "status")
+    if (!is.null(code) && code != 0L) {
+      cat(res, sep="\n")
+      stop("pdflatex failed with code ", code, ": message above")
+    }
+  }
+  run_bibtex <- function() {
+    args <- sub(".tex", "", texfile)
+    res <- suppressWarnings(system2(bibtex, args, stdout=TRUE, stderr=TRUE))
+    code <- attr(res, "status")
+    if (!is.null(code) && code != 0L) {
+      cat(res, sep="\n")
+      stop("bibtex failed with code ", code, ": message above")
+    }
+  }
+
+  run_latex()
+  run_bibtex()
+  run_latex()
+  run_latex()
+
   aux_files <- paste0(tools::file_path_sans_ext(texfile),
                      c(".log", ".aux", ".bbl", ".blg", ".fls", ".out",
                        ".fdb_latexmk"))
   file.remove(aux_files[file.exists(aux_files)])
+  invisible(NULL)
 }

5a92f5a6f213001c74f011e294e7020867fc100f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-06-02 11:45:41 +1000

    Copy Georges' doc over to use same approach as plant_paper
    Hope this is OK @kunstler; we'll probably move some of the other things
    over to use the same system, but this seems to scale quite well and
    is relatively easy to keep working.

--- R/utils.R
+++ R/utils.R
@@ -11,14 +11,3 @@ expand_range <- function(r, p) {
   d <- diff(r)
   r + d * c(-1, 1) * p
 }
-
-build_pdf_knit <- function(MD_file){
-
-    require(knitr)
-    name_file <- sub(".Rmd", "", MD_file)
-    knit(paste0(name_file, '.Rmd'))
-    args <- paste0(name_file, ".md", " -o ", name_file,".pdf")
-    print(args)
-    suppressWarnings(system2("pandoc", args, stdout=TRUE, stderr=TRUE))
-  }
-

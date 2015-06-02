ea5bdd1892d07c1446568f369ea77da3e778fc90
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-06-02 11:30:23 +1000

    Move to plant_paper build system
    Fixes #39

--- R/utils.R
+++ R/utils.R
@@ -12,50 +12,6 @@ expand_range <- function(r, p) {
   r + d * c(-1, 1) * p
 }
 
-build_pdf <- function(texfile) {
-  owd <- setwd("ms")
-  on.exit(setwd(owd))
-  texfile <- basename(texfile)
-
-  latex <- Sys.which("pdflatex")
-  bibtex <- Sys.which("bibtex")
-
-  if (latex == "" || bibtex == "") {
-    stop("latex / bibtex not found in path")
-  }
-
-  ## These can be generalised a bunch:
-  run_latex <- function() {
-    args <- c("-interaction=nonstopmode", texfile)
-    res <- suppressWarnings(system2(latex, args, stdout=TRUE, stderr=TRUE))
-    code <- attr(res, "status")
-    if (!is.null(code) && code != 0L) {
-      cat(res, sep="\n")
-      stop("pdflatex failed with code ", code, ": message above")
-    }
-  }
-  run_bibtex <- function() {
-    args <- sub(".tex", "", texfile)
-    res <- suppressWarnings(system2(bibtex, args, stdout=TRUE, stderr=TRUE))
-    code <- attr(res, "status")
-    if (!is.null(code) && code != 0L) {
-      cat(res, sep="\n")
-      stop("bibtex failed with code ", code, ": message above")
-    }
-  }
-
-  run_latex()
-  run_bibtex()
-  run_latex()
-  run_latex()
-
-  aux_files <- paste0(tools::file_path_sans_ext(texfile),
-                     c(".log", ".aux", ".bbl", ".blg", ".fls", ".out",
-                       ".fdb_latexmk"))
-  file.remove(aux_files[file.exists(aux_files)])
-  invisible(NULL)
-}
-
 build_pdf_knit <- function(MD_file){
 
     require(knitr)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# Competition kernels
+
 Ideas and code for the competition kernels.
+
+Requires:
+  - remake
+  - callr
+  - plant (>= 0.2.2)
